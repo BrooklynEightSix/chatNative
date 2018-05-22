7e61ecd9069ddbd1f3487954d182d8fe748d3276
William Lee <williamlee021@gmail.com>
2018-05-22 13:20:44 -0400

    add: NewMessageEntry stylesheets

--- Components/NewMessageEntry.js
+++ Components/NewMessageEntry.js
@@ -1,19 +1,27 @@
 import React, { Component } from 'react';
-import { AppRegistry, TextInput } from 'react-native';
+import { StyleSheet, AppRegistry, TextInput } from 'react-native';
 
 export default class UselessTextInput extends Component {
   constructor(props) {
     super(props);
-    this.state = { text: 'Useless Placeholder' };
+    this.state = { text: '' };
   }
 
   render() {
     return (
       <TextInput
-        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        style={styles.container}
         onChangeText={(text) => this.setState({text})}
         value={this.state.text}
       />
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'gray',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
